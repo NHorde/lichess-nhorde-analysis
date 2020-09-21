4c49e2cb7ea5cff9079a7af44541dd6a8e643d9a
nhorde <enhorde@gmail.com>
2020-09-21 15:41:51 -0400

    Call to API successful based on game ID

--- app/invocation/driver.py
+++ app/invocation/driver.py
@@ -1,1 +1,33 @@
-url = https://lichess.org/api/user/{username}/rating-history
+import requests
+import os
+import json
+
+api_key = os.environ.get('LICHESS_OAUTH_TOKEN')
+payload = {}
+
+# Make GET request to Lichess API
+resp = requests.request(
+    "GET",
+    url='https://lichess.org/game/export/4KxbUGJv',
+    data=payload,
+    params={
+        'pgnInJson': True,
+        'evals': False,
+        'clocks': False
+    }
+)
+
+# for i in vars(resp):
+#     print(i)
+
+resp_json = []
+ndjson = resp.content.decode().split('\n')
+
+# for json_obj in ndjson:
+#     if json_obj:
+#         resp_json.append(json.loads(json_obj))
+
+
+print(ndjson)
+
+
