ba4975ec3d04d5095473215539e070f5cba820ea
nhorde <enhorde@gmail.com>
2020-09-21 17:51:33 -0400

    Fixing request to use correct headers

--- app/invocation/driver.py
+++ app/invocation/driver.py
@@ -1,27 +1,52 @@
 import requests
 import os
+import json
+
+# api_key = os.environ.get('LICHESS_OAUTH_TOKEN')
+# payload = {}
+#
+# # Make GET request to Lichess API
+# resp = requests.request(
+#     "GET",
+#     url='https://lichess.org/api/games/user/NHorde',
+#     data=payload,
+#     params={
+#         'pgnInJson': True,
+#         'max': 2,
+#         'evals': False,
+#         'moves': True,
+#         'opening': True
+#     }
+# )
+#
+# resp_json = []
+# ndjson = resp.content.decode().split('\n')
+#
+# for json_obj in ndjson:
+#     if json_obj:
+#         resp_json.append(json_obj)
+# #
+# # for i in resp_json:
+# #     if "1. " in i:
+# #         print(i)
+#
+# print(resp.content)
 
 
-api_key = os.environ.get('LICHESS_OAUTH_TOKEN')
-payload = {}
 
-# Make GET request to Lichess API
-resp = requests.request(
-    "GET",
-    url='https://lichess.org/game/export/4KxbUGJv',
-    data=payload,
-    params={
-        'pgnInJson': True,
-        'evals': False,
-        'clocks': False
-    }
-)
+import requests, json
 
-resp_json = []
-ndjson = resp.content.decode().split('\n')
+headers = {
+    "accept":"application/x-ndjson"
+}
 
-for json_obj in ndjson:
-    if json_obj:
-        resp_json.append(json_obj)
+url = "https://www.lichess.org/api/games/user/nhorde"
 
-print(resp_json[-1])
+r = requests.get(url,
+                 params={"max":1, "perfType":"rapid","analysed":"false", "clocks":"false", "evals":"false", "opening":"true"},
+                 headers=headers)
+r_text = r.content.decode("utf-8")
+
+games = [json.loads(s) for s in r_text.split("\n")[:-1]]
+
+print(json.dumps(games, indent=3))
