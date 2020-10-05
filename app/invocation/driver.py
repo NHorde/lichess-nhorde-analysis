19bf569e497a05a8c4680b48e19b595af5df9f0e
Nicolas Horde <enhorde@gmail.com>
2020-10-05 09:58:51 -0400

    Refactoring driver

--- app/invocation/driver.py
+++ app/invocation/driver.py
@@ -1,8 +1,54 @@
 import setup
 from libs.state import State
-
-
 from services.manage import manager as manager_services
+from libs.logger import BASE_LOGGER
+
+LOGGER = BASE_LOGGER.getChild(__name__)
+
+def invoke(state: State):
+    """
+    Initialize parameters and make sure that correct type is provided
+
+    :param state: state
+    :type state: state
+    :return: function
+    :rtype: function
+    """
+
+    number_games = 5
+    start_date = 5
+    end_date = 5
+    color = "white"
+    evals = False
+
+    parameters = {
+        "number_games": number_games,
+        "start_date": start_date,
+        "end_date": end_date,
+        "color": color,
+        "evals": evals
+    }
+
+    return capture(state = state, parameters = parameters)
+
+def capture(state, parameters):
+    """
+    :param state: state
+    :type state: state
+    :param parameters: dict
+    :type parameters: dict
+    :return: function
+    :rtype: function
+    """
+    try:
+        state.client.capture(variables=parameters)
+        LOGGER.info("Parameters correctly initialized")
+
+    except Exception as e:
+        LOGGER.debug(f"Incorrect parameters, please check .env: {e}")
+
+    return manager_services()
+
 
 if __name__ == "__main__":
-    manager_services()
+    invoke(state=State())
--- app/services/lichess/manage.py
+++ app/services/lichess/manage.py
@@ -4,35 +4,7 @@ from libs.logger import BASE_LOGGER
 
 LOGGER = BASE_LOGGER.getChild(__name__)
 
-def initialization(state: State):
-    """
-    Initialize parameters and make sure that correct type is provided
 
-    :param state: state
-    :type state: state
-    :return: function
-    :rtype: function
-    """
-
-
-    number_games = 5
-    start_date = 5
-    end_date = 5
-    color = "white"
-    evals = False
-
-    parameters = {
-        "number_games": number_games,
-        "start_date": start_date,
-        "end_date": end_date,
-        "color": color,
-        "evals": evals
-    }
-
-    state.client.capture(variables=parameters)
-    LOGGER.info("Parameters correctly initialized")
-    return request_lichess_api()
 
 def manager():
-    LOGGER.info("Beginning of script")
-    initialization(state=State())
+    request_lichess_api()
