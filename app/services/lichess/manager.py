026ef294bc353b433ebd169f4e3f3c4cca7303fe
Nicolas Horde <enhorde@gmail.com>
2020-10-04 21:24:37 -0400

    Refactoring manager for Lichess

--- app/services/lichess/manager.py
+++ app/services/lichess/manager.py
@@ -1,5 +1,24 @@
 from services.lichess.request import request as request_lichess_api
+from libs.state import State
+from libs.logger import BASE_LOGGER
 
+LOGGER = BASE_LOGGER.getChild(__name__)
+
+def initialization(state: State):
+
+    number_games = 5
+    start_date = 5
+    end_date = 5
+
+    parameters = {
+        "number_games": number_games,
+        "start_date": start_date,
+        "end_date": end_date
+    }
+
+    state.client.capture(variables=parameters)
+
+    return request_lichess_api()
 
 def manager():
-    request_lichess_api()
+    initialization(state=State())
--- app/services/lichess/request.py
+++ app/services/lichess/request.py
@@ -1,25 +1,14 @@
 import requests
 import json
 
-from libs.state import State
 from libs.logger import BASE_LOGGER
 
 LOGGER = BASE_LOGGER.getChild(__name__)
 
 
 def request():
-    state = State()
-    number_games = 5
-    start_date = 5
-    end_date = 5
-    parameters = {
-        "number_games": 1,
-        "start_date": 1,
-        "end_date": 1
-    }
 
-    state.client.capture(variables=parameters)
-    exit(1)
+
     url = "https://www.lichess.org/api/games/user/nhorde"
 
     headers = {
