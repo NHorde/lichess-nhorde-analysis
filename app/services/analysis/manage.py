01d53986fd0d6926e162815bf42540d35ef821c8
Nicolas Horde <enhorde@gmail.com>
2020-10-06 14:14:23 -0400

    Starting plotting

--- app/services/analysis/manage.py
+++ app/services/analysis/manage.py
@@ -1,5 +1,6 @@
 from libs.state import State
 from services.analysis.processing import clean_data
+from services.analysis.plots import plot_models
 from libs.logger import BASE_LOGGER
 
 LOGGER = BASE_LOGGER.getChild(__name__)
@@ -21,6 +22,26 @@ def processing_data(state: State):
     except Exception as e:
         LOGGER.error(f"Could not process raw date: {e}")
 
+    return plot_graphs(state=state)
+
+
+def plot_graphs(state: State):
+    """
+    Plotting graphs of Lichess games
+
+    :param state: state
+    :type state: state
+    :return: state
+    :rtype: state
+    """
+
+    LOGGER.debug("Plotting graphs")
+
+    try:
+        plot_models(state=state)
+        LOGGER.debug("Plots created with success")
+    except Exception as e:
+        LOGGER.error(f"Could not plot graphs: {e}")
 
 def manager(state: State):
     """
---
+++ app/services/analysis/plots.py
@@ -0,0 +1,19 @@
+from libs.state import State
+from libs.logger import BASE_LOGGER
+
+LOGGER = BASE_LOGGER.getChild(__name__)
+
+def plot_opening_diversity(state: State):
+    """
+    Plot openings over the months
+
+    :param state: state
+    :type state: state
+    :return: plot
+    :rtype: plot
+    """
+    print("test")
+
+
+def plot_models(state: State):
+    plot_opening_diversity(state=state)
