ed7024b8e136145ab9422eb5859d7037b0552228
Nicolas Horde <enhorde@gmail.com>
2020-10-06 13:41:15 -0400

    Convert columns to lower

--- app/services/analysis/processing.py
+++ app/services/analysis/processing.py
@@ -3,6 +3,40 @@ from libs.logger import BASE_LOGGER
 
 LOGGER = BASE_LOGGER.getChild(__name__)
 
+def lower_columns(state: State):
+    """
+    Get everything lower case
+
+    :param state: state
+    :type state: state
+    :return: state
+    :rtype: state
+    """
+    LOGGER.debug("Appending year and month to dataframe")
+
+    try:
+        state.games.df.columns = map(str.lower, state.games.df.columns)
+        LOGGER.debug("Columns successfully coverted to lower cases")
+
+    except Exception as e:
+        LOGGER.error(f"Columns not converted to lower case: {e}")
+
+    return convert_date(state=state)
+
+def convert_date(state: State):
+    """
+    Add extra columns to handle dates more efficiently
+
+    :param state: state
+    :type state: state
+    :return: state
+    :rtype: state
+    """
+
+
+
+
+
 def clean_data(state: State):
     """
     Getting and processing data
@@ -12,5 +46,4 @@ def clean_data(state: State):
     :return: state
     :rtype: state
     """
-    print(state.games.df)
-    return
+    lower_columns(state=state)
