e879c4594e3ed548f46a85489925320385eb5914
Nicolas Horde <enhorde@gmail.com>
2020-10-06 12:22:34 -0400

    Adding analysis service

--- app/services/analysis/processing.py
+++ app/services/analysis/processing.py
@@ -4,4 +4,13 @@ from libs.logger import BASE_LOGGER
 LOGGER = BASE_LOGGER.getChild(__name__)
 
 def processing_data(state: State):
+    """
+    Getting and processing data
+
+    :param state: state
+    :type state: state
+    :return: state
+    :rtype: state
+    """
+    print(state.games.df)
     return
