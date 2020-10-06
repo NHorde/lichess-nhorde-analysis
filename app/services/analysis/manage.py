60f3de8b24b1aa4184c2b71e7d4e34c82ad58970
Nicolas Horde <enhorde@gmail.com>
2020-10-06 12:21:46 -0400

    Adding analysis service

--- app/services/analysis/manage.py
+++ app/services/analysis/manage.py
@@ -1,6 +1,34 @@
 from libs.state import State
+from services.analysis.processing import processing_data
+from libs.logger import BASE_LOGGER
+
+LOGGER = BASE_LOGGER.getChild(__name__)
+
+def processing_data(state: State):
+    """
+    Processing and cleaning data function
+    :param state: state
+    :type state: state
+    :return: state
+    :rtype: state
+    """
+    LOGGER.debug("Starting data processing")
+
+    try:
+        processing_data(state=state)
+        LOGGER.debug("Data processed with success")
+
+    except Exception as e:
+        LOGGER.error(f"Could not process raw date: {e}")
 
 
 def manager(state: State):
-    print(state.games.df)
-    return
+    """
+    Main function to call analysis service
+
+    :param state: state
+    :type state: state
+    :return: state
+    :rtype: state
+    """
+    processing_data(state=state)
---
+++ app/services/analysis/processing.py
@@ -0,0 +1,7 @@
+from libs.state import State
+from libs.logger import BASE_LOGGER
+
+LOGGER = BASE_LOGGER.getChild(__name__)
+
+def processing_data(state: State):
+    return
