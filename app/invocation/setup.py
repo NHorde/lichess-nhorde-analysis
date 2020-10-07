aa391ab2194f3017339573b8ae70dabcc8ee66d4
Nicolas Horde <enhorde@gmail.com>
2020-10-06 20:38:10 -0400

    Creating game diversity plot

--- app/invocation/setup.py
+++ app/invocation/setup.py
@@ -3,6 +3,5 @@ import sys
 
 PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.append(PATH)
 
-print(PATH)
+sys.path.append(PATH)
--- app/services/analysis/plots.py
+++ app/services/analysis/plots.py
@@ -10,7 +10,7 @@ LOGGER = BASE_LOGGER.getChild(__name__)
 
 def plot_game_type(state: State):
     """
-    Plot openings over the months
+    Plot game type over the months
 
     :param state: state
     :type state: state
@@ -39,6 +39,39 @@ def plot_game_type(state: State):
 
     except Exception as e:
         LOGGER.error(f"Plot 1 failed - Opening type - {e}")
+
+    return plot_opening_diversity(state=state)
+
+def plot_opening_diversity(state: State):
+    """
+    Plot opening diversity over the months
+
+    :param state: state
+    :type state: state
+    :return: plot
+    :rtype: plot
+    """
+    LOGGER.debug("Plotting game type over the months")
+
+    try:
+        plt.figure(figsize=(10, 5))
+        chart = sns.countplot(
+            data=state.games.df,
+            x='opening.eco'
+        )
+        chart.set_xticklabels(chart.get_xticklabels(),
+                              rotation=45,
+                              horizontalalignment='right',
+                              )
+        chart.invert_xaxis()
+        chart.set_title("Game diversity")
+        plt.xlabel("Eco")
+        plt.ylabel("# Games")
+
+        plt.savefig(ROOT_PATH + "/results/model_2.png")
+
+    except Exception as e:
+        LOGGER.error(f"Plot 2 failed - Opening diversity - {e}")
 
 def plot_models(state: State):
     plot_game_type(state=state)
