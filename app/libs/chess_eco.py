9a31369dfcf9de3f77ba97b6855b3885a86c0414
Nicolas Horde <enhorde@gmail.com>
2020-12-09 20:22:51 -0500

    Better with parenthesis

--- app/libs/chess_eco.py
+++ app/libs/chess_eco.py
@@ -14,18 +14,18 @@ def map_chess_eco(eco):
             opening_name = "Polish (Sokolsky) opening"
         if eco[1:3] == "01":
             opening_name = "Nimzovich-Larsen attack"
-        if int(eco[1:3]) in list(range(2,3):
+        if int(eco[1:3]) in list(range(2,3)):
             opening_name = "Bird's opening"
-        if int(eco[1:3]) in list(range(4,9):
+        if int(eco[1:3]) in list(range(4,9)):
             opening_name = "Reti opening"
-        if int(eco[1:3]) in list(range(10,39):
+        if int(eco[1:3]) in list(range(10,39)):
             opening_name = "English opening"
-        if int(eco[1:3]) in list(range(40,41):
+        if int(eco[1:3]) in list(range(40,41)):
             opening_name = "Queen's opening"
     else:
         opening_name = "Horde's opening"
     return opening_name
 
-print(map_chess_eco(eco="A04"))
-print(list(range(2,5)))
+print(map_chess_eco(eco="A34"))
+
 
